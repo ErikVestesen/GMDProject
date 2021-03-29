b5a463ccf4b4e5b66841a69460d4ef1ee61347a5
Erik Vestesen <erik.vestesen@hotmail.com>
2021-03-29 02:07:53 +0200

    added the game

---
+++ 2D RPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Movement goes here, once pr frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        movement.x = horizontal;
+        movement.y = vertical;
+       
+    }
+
+
+    //Fixed is for physics, default is 50 times pr second
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //rb.position is current position, add new vector and multiple it with speed and time 
+    }
+}
